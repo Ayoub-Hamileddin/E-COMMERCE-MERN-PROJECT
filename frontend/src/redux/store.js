438d5f2a23516855f75a8f457ddd2635102e0d4e
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-06-12 01:07:31 +0100

    add preloaded to favorites

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import authReducer from "./features/auth/authSlice";
+import favoriteReducer from "./features/favorites/FavoriteSlice.js";
+import { getFavoriteProduct } from "../Utils/LocalStorage.js";
+
+
+const initialFavorite = getFavoriteProduct();
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    favorites: favoriteReducer,
   },
+  preloadedState:{
+    favorites:initialFavorite,
+  }
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
