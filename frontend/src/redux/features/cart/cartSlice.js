59086bda45b1a11737e4fd65c09a81d5be857894
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-06-17 22:29:28 +0100

    create cartSlice for the store

---
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+
+
+const cartSlice=createSlice({
+ name:"cart",
+ initialState,
+ reducers:{
+    addToCart:(state,action)=>{
+            
+    }
+ }
+})
