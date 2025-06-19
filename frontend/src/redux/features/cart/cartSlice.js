827e15fa90fb48e4586a1bc4b18aa25a957c3bbb
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-06-19 08:31:01 +0100

    bug of remove

--- frontend/src/redux/features/cart/cartSlice.js
+++ frontend/src/redux/features/cart/cartSlice.js
@@ -22,7 +22,7 @@ const cartSlice = createSlice({
       return updateCart(state);
     },
     removeFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter((x) => x._id === action.payload);
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
     saveShippingAddress: (state, action) => {
