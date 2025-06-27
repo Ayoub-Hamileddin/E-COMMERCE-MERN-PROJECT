77e1bcc048e2555178ced5217b5a526d285874a5
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-06-27 11:59:17 +0100

    create route for the total orders - total sales -/mine

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,21 @@ import {
   authenticate,
   authorizedAdmin,
 } from "../middlewares/authMiddleware.js";
-import { createOrder } from "../controllers/orderController.js";
+import {
+  createOrder,
+  getAllorders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.route("/").post(authenticate, createOrder);
-
+router
+  .route("/")
+  .post(authenticate, createOrder)
+  .get(authenticate, authorizedAdmin, getAllorders);
+router.route("/mine").get(authenticate, getUserOrders);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
 export default router;
