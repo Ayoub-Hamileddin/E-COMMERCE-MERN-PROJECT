f566692aef57e6637b6e2d4537cddfc63089f4d9
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-01 08:51:16 +0100

    complete the shipping page order

---
+++ frontend/src/pages/Order/Shipping.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import {
+  saveShippingAddress,
+  savePaymentMethod,
+} from "../../redux/features/cart/cartSlice";
+import ProgressSteps from "../../components/ProgressSteps.jsx";
+
+const Shipping = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  const navigate = useNavigate();
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [adress, setAdress] = useState(shippingAddress.adress || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
+
+  useEffect(() => {
+    if (!shippingAddress.adress) {
+      navigate("/shipping");
+    }
+  }, [navigate, shippingAddress]);
+  return (
+    <div className="container mx-auto mt-10">
+      <ProgressSteps step1 step2 />
+      <div className="mt-[10rem] flex justify-around items-center flex-wrap">
+        <form className="w-[40rem]">
+          <h1 className="text-2xl font-serif font-semibold mb-4">Shipping :</h1>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Address</label>
+            <input
+              type="text"
+              value={adress}
+              onChange={(e) => setAdress(e.target.value)}
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter address"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2">City</label>
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter city"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2">PostalCode</label>
+            <input
+              type="text"
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter address"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Country</label>
+            <input
+              type="text"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter address"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Select Method</label>
+            <div className="mt-2">
+              <label className="inline-flex items-center">
+                <input
+                  type="radio"
+                  value={"PayPal"}
+                  name="paymentMethod"
+                  onChange={(e) => setPaymentMethod(e.target.value)}
+                  required
+                  className="form-radio text-pink-500"
+                  checked={paymentMethod === "PayPal"}
+                />
+                <span className="ml-2 text-black font-serif font-semibold">
+                  PayPal or Credit Card
+                </span>
+              </label>
+            </div>
+          </div>
+          <button
+            className="bg-pink-500 text-white py-2 px-4 rounded-full text-lg w-full"
+            type="submit"
+          >
+            Continue
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Shipping;
